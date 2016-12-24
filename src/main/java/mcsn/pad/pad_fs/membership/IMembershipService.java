c344cbd6ebfdddf63d72a6ea238fc930a8868f22
Leonardo Gazzarri <leogazza91@gmail.com>
2016-12-24 16:58:16 +0100

    interface for MembershipService

---
+++ src/main/java/mcsn/pad/pad_fs/membership/IMembershipService.java
@@ -0,0 +1,14 @@
+package mcsn.pad.pad_fs.membership;
+
+import java.util.List;
+import mcsn.pad.pad_fs.common.IService;
+
+public interface IMembershipService extends IService {
+	public Member getMyself();
+	
+	public List<Member> getMembers();
+	
+	public List<Member> getPreferenceList() throws InterruptedException;
+	
+	public Member getCoordinator(String key);
+}
