685aa41e610bf98cad58b3bed7ebbaa992aa0546
Leonardo Gazzarri <leogazza91@gmail.com>
2016-12-24 17:00:13 +0100

    added toString

--- src/main/java/mcsn/pad/pad_fs/message/Message.java
+++ src/main/java/mcsn/pad/pad_fs/message/Message.java
@@ -1,4 +1,4 @@
-package mcsn.pad.pad_fs.storage.remote;
+package mcsn.pad.pad_fs.message;
 
 import java.io.Serializable;
 
@@ -7,8 +7,22 @@ import voldemort.versioning.Versioned;
 public class Message implements Serializable {
 
 	private static final long serialVersionUID = 319471187021291131L;
+	
+	public static final int MAX_PACKET_SIZE = 102400;
+	
+	public static final int PUT 	= 0;
+	public static final int GET 	= 1;
+	public static final int LIST 	= 2;
+	public static final int REMOVE 	= 3;
+	public static final int SUCCESS = 4;
+	public static final int ERROR 	= 5;
+	public static final int UNKNOWN = 6;
+	public static final int OK 		= 4;
+	public static final int KO 		= 5;
 
 	public int type;
+	
+	public int status;
 
 	public Serializable key;
 
@@ -20,4 +34,8 @@ public class Message implements Serializable {
 		this.value = value;
 	}
  
+	@Override
+	public String toString() {
+		return "type: " + type + " ; key: " + key.toString() + " ; value: " + value.toString();
+	}
 }
