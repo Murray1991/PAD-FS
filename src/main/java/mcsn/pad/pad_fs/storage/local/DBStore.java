23616a24e3a5ae28aa428a146041bacb0f0ad29c
Leonardo Gazzarri <leogazza91@gmail.com>
2016-12-24 17:01:31 +0100

    subclass of LocalStore (TODO implement)

---
+++ src/main/java/mcsn/pad/pad_fs/storage/local/DBStore.java
@@ -0,0 +1,10 @@
+package mcsn.pad.pad_fs.storage.local;
+
+public class DBStore extends LocalStore {
+
+	public DBStore(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
