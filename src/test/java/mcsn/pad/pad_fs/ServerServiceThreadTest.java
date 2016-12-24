77c7974d234e544dc47460ca04a82aba7f7d17b5
Leonardo Gazzarri <leogazza91@gmail.com>
2016-12-24 17:01:52 +0100

    thread vrsion of ServerServiceTest

---
+++ src/test/java/mcsn/pad/pad_fs/ServerServiceThreadTest.java
@@ -0,0 +1,88 @@
+package mcsn.pad.pad_fs;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import mcsn.pad.pad_fs.message.Message;
+import mcsn.pad.pad_fs.server.ServerService;
+import mcsn.pad.pad_fs.utils.DummyService;
+import mcsn.pad.pad_fs.utils.TestUtils;
+import voldemort.versioning.Versioned;
+
+public class ServerServiceThreadTest {
+
+	@Test
+	public void test() throws IOException, ClassNotFoundException, InterruptedException  {
+
+		InetAddress addr = InetAddress.getByName("127.0.0.1");
+		// start the server
+		ServerService server = new ServerService(new DummyService(), 8080, 0, addr);
+		server.start();
+		
+		// build test elements
+		ArrayList<Versioned<byte[]>> list = (ArrayList<Versioned<byte[]>>) TestUtils.getElements(100);
+		
+		Executor executor = Executors.newFixedThreadPool(100);
+		CompletionService<Boolean> completionService = 
+		       new ExecutorCompletionService<Boolean>(executor);
+		
+		for ( Versioned<byte[]> e : list) {
+			Message sendMsg = new Message(1, TestUtils.nextSessionId(new SecureRandom()), e);
+			completionService.submit(new Callable<Boolean>() {
+				@Override
+				public Boolean call() throws Exception {
+					Socket sck = new Socket(addr, 8080);
+					Message rcvMsg = request(sendMsg, sck);
+					sck.close();
+					boolean b = 
+							sendMsg.key.equals(rcvMsg.key) &&
+							sendMsg.type == rcvMsg.type && 
+							! sendMsg.value.equals(rcvMsg.value);
+					return new Boolean(b);
+				}
+			});
+		}
+		
+		int errors = 0;
+		for(int received = 0; received < list.size() && errors == 0; received++) {
+	      Future<Boolean> resultFuture = completionService.take();
+	      Boolean result;
+	      try {
+			result = resultFuture.get();
+			Assert.assertTrue(result.booleanValue());
+	      } catch (ExecutionException e1) {
+			errors++;
+	      }
+		}
+		Assert.assertTrue(errors == 0);
+		server.shutdown();
+	}
+	
+	private Message request(Message sendMsg, Socket sck) throws IOException, ClassNotFoundException {
+		ObjectOutputStream oos = new ObjectOutputStream(sck.getOutputStream());
+		oos.writeObject(sendMsg);
+		
+		ObjectInputStream ois = new ObjectInputStream(sck.getInputStream());
+		Message msg = (Message) ois.readObject();
+		
+		ois.close();
+		oos.close();
+		return msg;
+	}
+
+}
