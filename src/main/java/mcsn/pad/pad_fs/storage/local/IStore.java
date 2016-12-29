c5928c31655d5b90b68b67668dd820fe58241135
Leonardo Gazzarri <leogazza91@gmail.com>
2016-12-29 18:48:47 +0100

    Store to IStore

---
+++ src/main/java/mcsn/pad/pad_fs/storage/local/IStore.java
@@ -0,0 +1,25 @@
+package mcsn.pad.pad_fs.storage;
+
+import voldemort.versioning.Versioned;
+
+/**
+ * 
+ * @author Leonardo Gazzarri
+ * 
+ * Basic interface that allows put, get and list operations.
+ * 
+ *
+ */
+public interface IStore<K,V> {
+	
+	public Versioned<V> get(K key);
+	
+	public void put(K key, Versioned<V> value);
+	
+	public void remove(K key);
+	
+	public Iterable<K> list();
+	
+	public void close();
+
+}
