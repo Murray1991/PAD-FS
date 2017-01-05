01f137acd22cde4fb753838848042c5c6807d789
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-05 18:37:16 +0100

    Added methods to the interfaces for management of vector clocks and other

--- src/main/java/mcsn/pad/pad_fs/storage/IStorageService.java
+++ src/main/java/mcsn/pad/pad_fs/storage/IStorageService.java
@@ -1,8 +1,33 @@
 package mcsn.pad.pad_fs.storage;
 
+import java.net.InetAddress;
+
 import mcsn.pad.pad_fs.common.IService;
 import mcsn.pad.pad_fs.message.ClientMessage;
+import mcsn.pad.pad_fs.storage.local.LocalStore;
+import voldemort.versioning.VectorClock;
 
 public interface IStorageService extends IService {
+	
 	public ClientMessage deliverMessage(ClientMessage msg);
+	
+	public int getID();
+	
+	/* synchronized method */
+	public VectorClock getVectorClock();
+	
+	/* synchronize method */
+	public VectorClock incrementVectorClock();
+	
+	/* synchronize method */
+	public VectorClock mergeAndIncrementVectorClock(VectorClock vc);
+	
+	/* synchronized method */
+	public VectorClock mergeVectorClock(VectorClock vc);
+	
+	public LocalStore getLocalStore();
+	
+	public int getStoragePort();
+	
+	public InetAddress getStorageAddress();
 }
