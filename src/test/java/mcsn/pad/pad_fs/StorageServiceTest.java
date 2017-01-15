fa43772a8d3421b8c46aa2eab217d80727356035
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-15 18:58:07 +0100

    Renamed test

--- src/test/java/mcsn/pad/pad_fs/StorageServiceTest.java
+++ src/test/java/mcsn/pad/pad_fs/StorageServiceTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -24,18 +25,11 @@ import mcsn.pad.pad_fs.storage.local.LocalStore;
 import mcsn.pad.pad_fs.utils.DummyLocalStore;
 import mcsn.pad.pad_fs.utils.TestUtils;
 
-public class StorageServiceTestWithUpdates {
-
-	@Test
-	public void test() throws FileNotFoundException, JSONException, IOException, InterruptedException {
-		
+public class StorageServiceTest {
+	
+	private void initServices(List<IService> mServices, List<IService> sServices, List<LocalStore> lStores, int dim) throws FileNotFoundException, JSONException, IOException, InterruptedException {
 		String filename = this.getClass().getResource("/gossip.conf").getFile();
 		File configFile = new File(filename);
-		
-		int dim = 4;
-		List<IService> mServices = new ArrayList<>();
-		List<IService> sServices = new ArrayList<>();
-		List<LocalStore> lStores = new ArrayList<>();
 		for (int i = 1 ; i <= dim; i++) {
 			Configuration config = new Configuration("127.0.0."+i, configFile);
 			
@@ -46,35 +40,38 @@ public class StorageServiceTestWithUpdates {
 			mServices.add( membershipService );
 			sServices.add( new StorageService(membershipService, localStore));
 		}
-		
-		System.out.println("-- start services");
+	}
+	
+	private void startServices(List<IService> mServices, List<IService> sServices) {
 		TestUtils.startServices(mServices);
 		TestUtils.startServices(sServices);
-		
-		System.out.println("-- wait few seconds");
-		Thread.sleep(6000);
-		
-		int num = 300;
-		System.out.println("-- build " + num + " messages and deliver them);");
+	}
+	
+	private void shutdownServices(List<IService> mServices, List<IService> sServices) {
+		TestUtils.shutdownServices(mServices);
+		TestUtils.shutdownServices(sServices);
+	}
+	
+	private Map<Serializable, ClientMessage> createMessages(int num) {
 		Map<Serializable, ClientMessage> map = new HashMap<>();
 		for (int i=0; i<num; i++) {
 			ClientMessage msg = new ClientMessage(Message.PUT, TestUtils.getRandomString(), 
 					TestUtils.getVersioned(TestUtils.getRandomString().getBytes()));
-			int idx = (Math.abs(msg.key.hashCode()) % dim) + 1;
-			((StorageService) sServices.get(idx-1)).deliverMessage(msg);
 			map.put(msg.key, msg);
 		}
-		
-		int delta = 20;
-		System.out.println("-- wait " + delta + " seconds");
-		Thread.sleep(20*1000);
-		
-		for (Serializable key : map.keySet() ) {
-			Assert.assertTrue( TestUtils.existKey(key, lStores) );
+		return map;
+	}
+	
+	private void deliverMessages(Map<Serializable, ClientMessage> map, List<IService> sServices) {
+		Iterator<Serializable> it = map.keySet().iterator();
+		while (it.hasNext()) {
+			ClientMessage msg = map.get(it.next());
+			int idx = (Math.abs(msg.key.hashCode()) % sServices.size()) + 1;
+			((StorageService) sServices.get(idx-1)).deliverMessage(msg);
 		}
-		System.out.println("--- keys exist!");
-		
-		System.out.println("-- update values");
+	}
+	
+	private void updateAndDeliverMessages(Map<Serializable, ClientMessage> map, List<IService> sServices) {
 		Random rand = new Random();
 		for (Serializable key : map.keySet()) {
 			if (rand.nextInt(10) <= 3) {
@@ -87,22 +84,17 @@ public class StorageServiceTestWithUpdates {
 									)
 						);
 				map.put(key, msg);
-				int idx = (Math.abs(msg.key.hashCode()) % dim) + 1;
+				int idx = (Math.abs(msg.key.hashCode()) % sServices.size()) + 1;
 				((StorageService) sServices.get(idx-1)).deliverMessage(msg);
 			}
 		}
-		
-		System.out.println("-- wait " + delta + " seconds");
-		Thread.sleep(20*1000);
-		
-		System.out.println("-- shutdown services");
-		TestUtils.shutdownServices(mServices);
-		TestUtils.shutdownServices(sServices);
-		
+	}
+	
+	private void checkValues(Map<Serializable, ClientMessage> map, List<LocalStore> lStores){
 		for (Serializable key : map.keySet() ) {
 			int stores = TestUtils.countKey(key, lStores);
 			
-			Assert.assertTrue( stores == dim );
+			Assert.assertTrue( stores == lStores.size() );
 			
 			Assert.assertTrue( 
 					
@@ -126,9 +118,40 @@ public class StorageServiceTestWithUpdates {
 							) 
 					); 
 		}
+	}
+
+	@Test
+	public void testWithUpdates() throws FileNotFoundException, JSONException, IOException, InterruptedException {
 		
+		List<IService> mServices = new ArrayList<>();
+		List<IService> sServices = new ArrayList<>();
+		List<LocalStore> lStores = new ArrayList<>();
+		
+		initServices(mServices, sServices, lStores, 4);
+		startServices(mServices, sServices);
+		
+		System.out.println("-- Wait...");
+		Thread.sleep(6000);
+		
+		Map<Serializable, ClientMessage> map = createMessages(300);
+		
+		System.out.println("-- Deliver Messages to the Services");
+		deliverMessages(map, sServices);
+		
+		System.out.println("-- Wait...");
+		Thread.sleep(20*1000);
+		
+		System.out.println("-- Update values");
+		updateAndDeliverMessages(map, sServices);
+		
+		System.out.println("-- Wait...");
+		Thread.sleep(20*1000);
+		
+		System.out.println("-- Shutdown all services");
+		shutdownServices(mServices, sServices);
+		
+		checkValues(map, lStores);
 		System.out.println("--- values are OK!");
 		
 	}
-
 }
