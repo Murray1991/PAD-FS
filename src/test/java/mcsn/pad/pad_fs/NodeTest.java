02c34c8c20fdcb7427cb0e572011196851e03d9e
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-20 12:10:26 +0100

    merged NodeTest and NodeFailureTest

--- src/test/java/mcsn/pad/pad_fs/NodeTest.java
+++ src/test/java/mcsn/pad/pad_fs/NodeTest.java
@@ -7,50 +7,76 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.json.JSONException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class NodeTest {
 	
-
-	public int dim = 4;
-
-	@Test
-	public void test() throws InterruptedException, FileNotFoundException, JSONException, IOException {
+	private final int size = 4;
+	private List<Node> nodes = new ArrayList<>();
+	private int dim = size;
+	
+	@Before
+	public void setup() throws FileNotFoundException, JSONException, IOException, InterruptedException {
+		System.out.println("-- setup NodeTest");
+		String filename = this.getClass().getResource("/gossip.conf").getFile();
+		File configFile = new File(filename);
+		this.nodes = new ArrayList<>();
 		
-			String filename = this.getClass().getResource("/gossip.conf").getFile();
-			File configFile = new File(filename);
-			List<Node> members = new ArrayList<>();
-			
-			for (int i = 1; i <= dim; i++) {
-				members.add(i-1,  new Node("127.0.0."+i, configFile));
-				
-			}
-			
-			members.add(dim++, new Node("127.0.0.10", configFile));
-			members.add(dim++, new Node("127.0.0.11", configFile));
-			
-			System.out.println("starting nodes...");
-			for (Node node : members)
-				node.start();
-			
-			Thread.sleep(10000);
-			
-			for (int i = 0; i < dim; i++) {
-				int x = members.get(i).getMemberList().size();
-				
-				System.out.println("#members = " + x);
-				assertTrue("gossip failed: " + x + " members instead of " + (dim-1) + " expected", x == dim-1);
-			}
-			
-			
-			assertTrue(true);
-			
-			for (Node n : members)
-				n.shutdown();
+		for (int i = 1; i <= dim; i++) {
+			nodes.add(i-1,  new Node("127.0.0."+i, configFile));
+		}
 		
+		nodes.add(dim++, new Node("127.0.0.10", configFile));
+		nodes.add(dim++, new Node("127.0.0.11", configFile));
+		
+		System.out.println("starting nodes...");
+		for (Node node : nodes)
+			node.start();
+		
+		Thread.sleep(10000);
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("-- teardown NodeTest");
+		dim = size;
+		for (Node n : nodes)
+			n.shutdown();
 	}
 
+	@Test
+	public void nodeGossipTest() throws InterruptedException, FileNotFoundException, JSONException, IOException {
+			for (int i = 0; i < dim; i++) {
+				int x = nodes.get(i).getMemberList().size();
+				assertTrue("#nodes: " + x, x == dim-1);
+			}
+	}
+	
+	@Test
+	public void nodeFailureGossipTest() throws InterruptedException, FileNotFoundException, JSONException, IOException {
+		
+		/* delete two nodes at random */
+		Random rand = new Random();
+		int index = rand.nextInt(dim--);
+		nodes.get(index).shutdown();
+		nodes.remove(index);
+		
+		index = rand.nextInt(dim--);
+		nodes.get(index).shutdown();
+		nodes.remove(index);
+		
+		Thread.sleep(20000);
+		
+		/* we should have 3 nodes for node */
+		for (int i = 0; i < dim; i++) {
+			int x = nodes.get(i).getMemberList().size();
+			assertTrue("#nodes = " + x, x == dim-1);
+		}
+	}
 }
