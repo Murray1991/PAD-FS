9f9ea0c7c79c8b21837e2ddfd48637a15c5a2e7e
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-22 22:20:05 +0100

    fixed problem for a restart calling start method

--- src/main/java/mcsn/pad/pad_fs/storage/StorageService.java
+++ src/main/java/mcsn/pad/pad_fs/storage/StorageService.java
@@ -1,12 +1,16 @@
 package mcsn.pad.pad_fs.storage;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.lang.Thread.State;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.util.logging.Logger;
 
 import junit.framework.Assert;
+import mcsn.pad.pad_fs.common.Utils;
 import mcsn.pad.pad_fs.membership.IMembershipService;
 import mcsn.pad.pad_fs.membership.Member;
 import mcsn.pad.pad_fs.message.ClientMessage;
@@ -20,25 +24,32 @@ public class StorageService implements IStorageService {
 	
 	private final static Logger LOGGER = Logger.getLogger(StorageService.class.getName());
 	
-	private boolean isRunning;
+	private boolean isRunning = false;
 	private StorageManager storageManager;
 	private ReplicaManager replicaManager;
 	private IMembershipService membershipService;
 	private LocalStore localStore;
-	private final int storageManagerPort = 3000; //TODO
+	private LocalStore concurrentLocalStore;
+	//TODO See if I can set the storageManagerPort in the config file
+	private final int storageManagerPort = 3000; 
 
 	private VectorClock vectorClock;
 
 	private int ID;
 
+	private String host;
+
+
 	public StorageService(IMembershipService membershipService, LocalStore localStore) {
+		Assert.assertNotNull(localStore.getNext());
 		this.membershipService = membershipService;
 		this.localStore = localStore;
-		String host = membershipService.getMyself().host;
+		this.concurrentLocalStore = localStore.getNext();
+		this.host = membershipService.getMyself().host;
 		storageManager = new StorageManager(this, host, storageManagerPort);
 		replicaManager = new ReplicaManager(this, membershipService, 1000, host, storageManagerPort);
 		
-		//TODO check better ways to do ID...
+		//TODO Maybe better way to get an ID
 		this.ID = Math.abs(host.hashCode()) % Short.MAX_VALUE;
 		
 		//Vector clock...
@@ -49,18 +60,30 @@ public class StorageService implements IStorageService {
 	@Override
 	public void start() {
 		LOGGER.info(this + " -- start");
-		isRunning = true;
-		storageManager.start();
-		replicaManager.start();
+		State stateS = storageManager.getState();
+		State stateR = replicaManager.getState();
+		if (! stateS.equals(State.NEW) ) {
+			storageManager = new StorageManager(this, host, storageManagerPort);
+		}
+		if (! stateR.equals(State.NEW)) {
+			replicaManager = new ReplicaManager(this, membershipService, 1000, host, storageManagerPort);
+		}
+		if (!isRunning) {
+			isRunning = true;
+			storageManager.start();
+			replicaManager.start();
+		}
 	}
 	
 	@Override
 	public void shutdown() {
 		LOGGER.info(this + " -- shutdown");
-		isRunning = false;
-		localStore.close(); //TODO check
-		storageManager.interrupt();
-		replicaManager.interrupt();
+		if (isRunning) {
+			isRunning = false;
+			replicaManager.interrupt();
+			storageManager.interrupt();
+			localStore.close(); 
+		}
 	}
 
 	@Override
@@ -68,31 +91,29 @@ public class StorageService implements IStorageService {
 		return isRunning;
 	}
 
-	//TODO maybe better name is "HandleRequest"
 	@Override
 	public ClientMessage deliverMessage(ClientMessage msg) {
-		Member coordinator = membershipService.getCoordinator((String) msg.key); //TODO check
+		Member coordinator = membershipService.getCoordinator(
+				msg.key != null ? (String) msg.key : "");
 		Member myself = membershipService.getMyself();
 		ClientMessage rcvMsg = null;
-		
-		Assert.assertTrue( coordinator != null);
-		Assert.assertTrue( myself != null);
-		
-		if (coordinator.equals(myself)) {
+		if (coordinator.equals(myself) || msg.type == Message.LIST) {
 			resolveMessage(msg);
 			rcvMsg = msg;
 		} else {
+			DatagramSocket socket = null;
 			try {
-				DatagramSocket socket = new DatagramSocket();
+				socket = new DatagramSocket();
 				Transport transport = new Transport(socket, this);
 				transport.send(msg, new InetSocketAddress(coordinator.host, storageManagerPort)); 
-				//UDP.send(msg, socket, new InetSocketAddress(coordinator.host, storageManagerPort)); 
 				rcvMsg = (ClientMessage) transport.receive().msg; 
-				//TODO check if rcvMsg is correct
-				//TODO what if I don't receive the message? Do something with timeouts?
-				socket.close();
+				//TODO check if rcvMsg is correct and handle 
+				//the case in which I don't receive the message (timeouts)
 			} catch (IOException | ClassNotFoundException e) {
 				e.printStackTrace();
+			} finally {
+				if (socket != null) 
+					socket.close();
 			}
 		}
 		return rcvMsg;
@@ -105,19 +126,19 @@ public class StorageService implements IStorageService {
 	
 	@Override
 	public synchronized VectorClock mergeVectorClock(VectorClock vc) {
-		vectorClock.merge(vc);
+		vectorClock = vectorClock.merge(vc);
 		return vectorClock.clone();
 	}
 	
 	@Override
-	public VectorClock incrementVectorClock() {
+	public synchronized VectorClock incrementVectorClock() {
 		vectorClock.incrementVersion(getID(), System.currentTimeMillis());
 		return vectorClock.clone();
 	}
 
 	@Override
-	public VectorClock mergeAndIncrementVectorClock(VectorClock vc) {
-		vectorClock.merge(vc);
+	public synchronized VectorClock mergeAndIncrementVectorClock(VectorClock vc) {
+		vectorClock = vectorClock.merge(vc);
 		vectorClock.incrementVersion(getID(), System.currentTimeMillis());
 		return vectorClock.clone();
 	}
@@ -152,19 +173,46 @@ public class StorageService implements IStorageService {
 		switch (msg.type) {
 			case Message.GET:
 				msg.value = localStore.get(msg.key);
+				msg.addValue(concurrentLocalStore.get(msg.key));
+				if (msg.value == null)
+					msg.status = Message.NOT_FOUND;
 				break;
 			case Message.PUT:
-				/* only Coordinator put the versioned value in the local store */
+				/* only the coordinator put the version
+				 * in the local store. TODO check if ok... */
 				msg.value = new Versioned<byte[]>(
 						msg.value.getValue(), 
 						incrementVectorClock());
 				localStore.put(msg.key, msg.value);
+				concurrentLocalStore.remove(msg.key);
 				break;
 			case Message.REMOVE:
-				localStore.remove(msg.key);
-				break;	
+				/* remove only if the msg's version
+				 * is greater than the local value's
+				 * version but multicast anyway if
+				 * removeFlag is true */
+				Versioned<byte[]> v1= msg.value;
+				Versioned<byte[]> v2 = localStore.get(msg.key);
+				if ( Utils.compare(v1, v2) == 1 ) {
+					localStore.remove(msg.key);
+					concurrentLocalStore.remove(msg.key);
+				} 
+				if (msg.removeFlag) {
+					msg.removeFlag = false;
+					DatagramSocket sck = null;
+					try {
+						sck = new DatagramSocket();
+						new Transport(sck, this).multicast(msg, membershipService.getMembers());
+					} catch (SocketException e) {
+					} finally {
+						if (sck != null)
+							sck.close();
+					}
+				}
+				break;
 			case Message.LIST:
-				//TODO implement LIST...
+				for (Serializable key : localStore.list())
+					msg.addKey(key);
 				break;
 			default:
 				msg.status = Message.ERROR;
