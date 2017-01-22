65ec8f0946e5517ff0d92fcb0b3662208ea10394
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-22 21:33:42 +0100

    new constructors taking as parameter a subclass of LocalStore

--- src/main/java/mcsn/pad/pad_fs/Node.java
+++ src/main/java/mcsn/pad/pad_fs/Node.java
@@ -3,12 +3,15 @@ package mcsn.pad.pad_fs;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.json.JSONException;
+import org.junit.Assert;
 
 import mcsn.pad.pad_fs.common.Configuration;
 import mcsn.pad.pad_fs.common.IService;
@@ -16,6 +19,7 @@ import mcsn.pad.pad_fs.membership.Member;
 import mcsn.pad.pad_fs.membership.MembershipService;
 import mcsn.pad.pad_fs.storage.StorageService;
 import mcsn.pad.pad_fs.storage.local.DBStore;
+import mcsn.pad.pad_fs.storage.local.LocalStore;
 
 /**
  * 
@@ -33,23 +37,37 @@ public class Node
 	private List<IService> services;
 
 	private MembershipService membershipService;
+	
+	private Node(Configuration config, Class<? extends LocalStore> storeClass) throws UnknownHostException, InterruptedException {
+		this.config = config;
+		this.services = createServices(storeClass);
+	}
     
     public Node(File configFile) throws FileNotFoundException, JSONException, IOException, InterruptedException {
-    	config = new Configuration(configFile);
-    	services = createServices();
+    	this( new Configuration(configFile) , DBStore.class );
 	}
     
 	public Node(final int port, String seed) throws UnknownHostException, InterruptedException {
-		config = new Configuration(seed, port);
-		services = createServices();
+		this( new Configuration(seed, port) , DBStore.class  );
 	}
 
 	public Node(String ipAddress, File configFile) throws FileNotFoundException, JSONException, IOException, InterruptedException {
-		config = new Configuration (ipAddress, configFile);
-		services = createServices();
+		this( new Configuration (ipAddress, configFile) , DBStore.class  );
 	}
 	
-	private List<IService> createServices() throws UnknownHostException, InterruptedException {
+	public Node(File configFile, Class<? extends LocalStore> storeClass) throws FileNotFoundException, JSONException, IOException, InterruptedException {
+    	this( new Configuration(configFile) , storeClass );
+	}
+    
+	public Node(final int port, String seed, Class<? extends LocalStore> storeClass) throws UnknownHostException, InterruptedException {
+		this( new Configuration(seed, port) , storeClass  );
+	}
+
+	public Node(String ipAddress, File configFile, Class<? extends LocalStore> storeClass) throws FileNotFoundException, JSONException, IOException, InterruptedException {
+		this( new Configuration (ipAddress, configFile) , storeClass  );
+	}
+	
+	private List<IService> createServices(Class<? extends LocalStore> storeClass) throws UnknownHostException, InterruptedException {
 		List<IService> services = new ArrayList<>();
 		membershipService = 
 				new MembershipService( config.getHost(),
@@ -60,7 +78,24 @@ public class Node
 		
 		services.add(membershipService);
 		
-		DBStore dbStore = new DBStore("thisthis");
+		LocalStore dbStore = null;
+		try {
+			
+			Constructor<?> constructor = storeClass.getConstructor(String.class);
+			dbStore = (LocalStore) constructor.newInstance( 
+							new Object[] { config.getHost() + ".db" });
+			
+		} catch (InstantiationException 
+				| IllegalAccessException 
+				| IllegalArgumentException 
+				| InvocationTargetException
+				| NoSuchMethodException 
+				| SecurityException e) {
+			e.printStackTrace();
+		}
+		
+		Assert.assertNotNull("local store is null", dbStore);
+		
 		StorageService storageService = 
 				new StorageService(membershipService, dbStore);
 		
