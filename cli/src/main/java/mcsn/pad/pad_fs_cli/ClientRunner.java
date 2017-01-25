28910317efbfef559e2ffd8fdfbfa5a631fca309
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-25 22:30:29 +0100

    moved parsing in OptionManager class

--- cli/src/main/java/mcsn/pad/pad_fs_cli/ClientRunner.java
+++ cli/src/main/java/mcsn/pad/pad_fs_cli/ClientRunner.java
@@ -1,55 +1,107 @@
 package mcsn.pad.pad_fs_cli;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.apache.commons.cli.ParseException;
+
+import com.sun.mail.iap.ConnectionException;
+
+import mcsn.pad.pad_fs.message.ClientMessage;
+import mcsn.pad.pad_fs.message.Message;
 
 public class ClientRunner {
 	
     public static void main( String[] args ) throws Exception {
-    	System.out.println( "Hello World!" );
+    	
+    	Random rand = new Random();
+    	OptionManager om = new OptionManager();
 
-        Options options = new Options();
-        
-        Option interactive = new Option("i", "interactive", false, "interactive mode");
-        options.addOption(interactive);
-        
-        Option config = new Option("c", "config", true, "configuration file in which are stored the ip addresses of the pad-fs nodes");
-        options.addOption(config);
-        
-        Option dest = new Option("d", "dest", true, "specify the ip addr of the node where to send the requeste");
-        options.addOption(dest);
-        
-        Option get = new Option("g", "get", true, "get value associated to a key from the pad-fs");
-        options.addOption(get);
-        
-        Option put = new Option("p", "put", true, "put <key,value> or a file in the pad-fs");
-        options.addOption(put);
-        
-        Option remove = new Option("r", "remove", true, "delete a key");
-        options.addOption(remove);
-        
-        Option list = new Option("l", "list", false, "list all the keys for a node");
-        options.addOption(list);
-        
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = parser.parse(options, args);
-        if (cmd == null) {
-        	formatter.printHelp("utility-name", options);
-            System.exit(1);
-            return;  
-        }
+    	try {
+			om.parse(args);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+        	om.printHelp("pad-fs-cli");
+        	System.exit(1);
+        	return;
+		}
+    	
+    	int type = 0;
+        String[] values = null;
+    	ArrayList<InetSocketAddress> addresses = om.getAddressPool();
 
-        String putV = cmd.getOptionValue("input");
-        //String outputFilePath = cmd.getOptionValue("output");
-
-        //System.out.println(inputFilePath);
-        //System.out.println(outputFilePath);
-        System.out.println(putV);
-        
+    	for (boolean run = true; run; ) {
+    		if (om.isInteractive()) {
+        		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            	String line = br.readLine();
+            	if (line.equals("q") || line.equals("quit"))
+            		break;
+            	
+            	String[] parts = line.split("\\W+", 2);
+            	type = om.getType(parts[0]);
+            	values = parts[1].split("\\W+");
+            	br.close();
+        	} 
+        	
+        	if (! om.isInteractive()){
+        		type = om.getType();
+        		values = om.getValues();
+        		run = false;
+        	}
+        	
+        	ClientMessage rcvMsg = null;
+        	int size = addresses.size();
+        	int idx = rand.nextInt(size);
+        	
+        	for (int i = 0; i < addresses.size() && rcvMsg == null; i++ ) {
+        		try {
+        			InetSocketAddress raddr = addresses.get( (idx+i) % size );
+        			rcvMsg = new Client(raddr).send(type, values);
+                	processMessage(rcvMsg, om.getOutputPathFile());
+                } catch (IOException | ClassNotFoundException e){
+    			}
+        	}
+    	}
     }
+
+	private static void processMessage(ClientMessage msg, String outputPathFile) {
+		
+		if (msg.status == Message.ERROR) {
+        	System.err.println("Error: remote failure, please retry");
+        }
+		
+        if (msg.status == Message.NOT_FOUND) {
+        	System.out.println("Your request is not present in the pad-fs system");
+        }
+        
+        byte[] data = null;
+        if (msg.type == Message.GET && msg.status == Message.SUCCESS ) {
+        	data = msg.value.getValue();
+        }
+        
+        try {
+        	
+	        if (data != null && outputPathFile != null) {
+	        	System.out.println("saving in \"" + outputPathFile + "\"");
+	        	new File(outputPathFile).createNewFile();
+	        	Files.write(Paths.get(outputPathFile), data);
+	        }
+	        
+	        if (data != null && outputPathFile == null) {
+	        	System.out.println(new String(data));
+	        }
+	        
+        } catch (IOException e) {
+			System.err.println("Impossible saving in " + outputPathFile);
+		}
+		
+	}
 }
