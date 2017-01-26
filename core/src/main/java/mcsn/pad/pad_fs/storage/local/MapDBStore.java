6367c72beff8ad9a1e20d78550ba3145e189b569
Leonardo Gazzarri <leogazza91@gmail.com>
2017-01-26 12:24:02 +0100

    implemented persistent storage using mapdb

--- core/src/main/java/mcsn/pad/pad_fs/storage/local/MapDBStore.java
+++ core/src/main/java/mcsn/pad/pad_fs/storage/local/MapDBStore.java
@@ -1,9 +1,62 @@
 package mcsn.pad.pad_fs.storage.local;
 
-public class DBStore extends LocalStore {
-	//TODO implement the DBStore
-	public DBStore(String name) {
-		super(name);
+import java.io.Serializable;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import voldemort.versioning.Versioned;
+
+public class MapDBStore extends LocalStore {
+	
+	private DB db;
+	private HTreeMap<Serializable, Versioned<byte[]>> map;
+	
+	@SuppressWarnings("unchecked")
+	private MapDBStore(String name, String append) {
+		super(name+append);
+		db = DBMaker.fileDB(name+append).make();
+		map = (HTreeMap<Serializable, Versioned<byte[]>>) 
+				db.hashMap("concurrent").createOrOpen();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public MapDBStore(String name) {
+		super(name, new MapDBStore(name, ".concurrent"));
+		db = DBMaker.fileDB(name).make();
+		map = (HTreeMap<Serializable, Versioned<byte[]>>) 
+				db.hashMap("principal").createOrOpen();
+	}
+
+	@Override
+	public Versioned<byte[]> get(Serializable key) {
+		return (Versioned<byte[]>) map.get(key);
+	}
+
+	@Override
+	public void put(Serializable key, Versioned<byte[]> value) {
+		map.put(key, value);
+		db.commit();
+	}
+	
+	@Override
+	public void remove(Serializable key) {
+		map.remove(key);
+		db.commit();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterable<Serializable> list() {
+		return map.keySet();
+	}
+	
+	@Override
+	public int size() {
+		return map.keySet().size();
+	}
+	
+	@Override
+	public void close() {
 	}
 
 }
