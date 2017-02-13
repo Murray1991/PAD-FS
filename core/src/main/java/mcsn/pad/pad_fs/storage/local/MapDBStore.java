d5d91d4ee758274d5638d943b0917c38ad382d89
Leonardo Gazzarri <leogazza91@gmail.com>
2017-02-13 12:00:08 +0100

    multiple readers one writer

--- core/src/main/java/mcsn/pad/pad_fs/storage/local/MapDBStore.java
+++ core/src/main/java/mcsn/pad/pad_fs/storage/local/MapDBStore.java
@@ -3,9 +3,13 @@ package mcsn.pad.pad_fs.storage.local;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import org.mapdb.BTreeMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+
 import mcsn.pad.pad_fs.common.Utils;
 import voldemort.versioning.VectorClock;
 import voldemort.versioning.Versioned;
@@ -13,12 +17,9 @@ import voldemort.versioning.Versioned;
 public class MapDBStore extends LocalStore {
 	
 	private DB db;
-	private BTreeMap<Serializable, List<Versioned<byte[]>>> map;
+	private HTreeMap<Serializable, List<Versioned<byte[]>>> map;
 	
-	/*
-	private DB dbCache;
-	private HTreeMap<Serializable, List<Versioned<byte[]>>> mapCache;
-	*/
+    private ReadWriteLock rwLock = new ReentrantReadWriteLock();
 	
 	@SuppressWarnings("unchecked")
 	public MapDBStore(String name) {
@@ -33,66 +34,65 @@ public class MapDBStore extends LocalStore {
 				.make();
 		
 		@SuppressWarnings("rawtypes")
-		BTreeMap map = (BTreeMap<Serializable, List<Versioned<byte[]>>>) db
-				.treeMap("principal")
+		HTreeMap map = (HTreeMap<Serializable, List<Versioned<byte[]>>>) db
+				.hashMap("principal")
 				.counterEnable()
 				.createOrOpen();
 		
 		this.map = map;
-		
-		/* faster approach but it does not commit the inDisk files
-		 * TODO investigate..
-		 
-		dbCache = DBMaker
-				.memoryDB()
-				.make();
-		
-		mapCache = dbCache
-				.hashMap("inMemory")
-				.expireOverflow(map)
-				.expireAfterCreate(1000)
-				.expireAfterUpdate(1000)
-				.expireAfterGet(1000)
-				//.expireMaxSize(3*1024*1024) //3MB
-				.expireExecutor(Executors.newScheduledThreadPool(2))
-				.expireExecutorPeriod(1000)
-				.create();
-				
-		*/
 	}
 
 	@Override
 	public List<Versioned<byte[]>> get(Serializable key) {
-		return map.get(key);
+		List<Versioned<byte[]>> res = null;
+		rwLock.readLock().lock();
+		try {
+			res = map.get(key);
+		} finally {
+			rwLock.readLock().unlock();
+		}
+		return res;
 	}
 
 	@Override
-	synchronized public void put(Serializable key, Versioned<byte[]> value) {
-		if ( pput(key, value) ) {
-			db.commit();
+	public void put(Serializable key, Versioned<byte[]> value) {
+		rwLock.writeLock().lock();
+		try {
+			if ( pput(key, value) ) {
+				db.commit();
+			}
+		} finally {
+			rwLock.writeLock().unlock();
 		}
 	}
 	
 	/* fast fix for reply handler, it's ugly but doesn't commit for every put */
-	synchronized public void put(List<Serializable> keys, List<Versioned<byte[]>> values) {
+	public void put(List<Serializable> keys, List<Versioned<byte[]>> values) {
 		if (keys.size() != values.size()) {
 			throw new RuntimeException("put error");
 		}
 		
-		boolean b = false;
-		for (int i=0; i<keys.size(); i++) {
-			Serializable key = keys.get(i);
-			Versioned<byte[]> value = values.get(i);
-			b = b || pput(key, value);
-		}
-		
-		if (b) {
-			db.commit();
+		rwLock.writeLock().lock();
+		try {
+			
+			boolean b = false;
+			for (int i=0; i<keys.size(); i++) {
+				Serializable key = keys.get(i);
+				Versioned<byte[]> value = values.get(i);
+				b = b || pput(key, value);
+			}
+			
+			if (b) {
+				db.commit();
+			}
+			
+		} finally {
+			rwLock.writeLock().unlock();
 		}
 	}
 	
 	@Override
-	synchronized public void remove(Serializable key, VectorClock vc) {
+	public void remove(Serializable key, VectorClock vc) {
 		put(key, new Versioned<byte[]>(null, vc));
 	}
 
