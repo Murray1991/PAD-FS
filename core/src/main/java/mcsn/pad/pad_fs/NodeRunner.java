0cb3e394327071f0cc02302f9af23b554fec4971
Leonardo Gazzarri <leogazza91@gmail.com>
2017-02-15 16:25:38 +0100

    ActiveMapDBStore is the default store

--- core/src/main/java/mcsn/pad/pad_fs/NodeRunner.java
+++ core/src/main/java/mcsn/pad/pad_fs/NodeRunner.java
@@ -49,14 +49,14 @@ public class NodeRunner {
 		Option bindAddr = new Option("b", "bindaddr", true, "binding address");
 		Option path = new Option("p", "path", true, "the path in which store/retrieve the DBs");
 		Option hmStore = new Option("h", "hmstore", false, "specifies to use the in-memory HashMap store");
-		Option activeStore = new Option("a", "active", false, "specifies to use the ActiveMapDBStore store");
+		Option mdbStore = new Option("m", "mdbstore", false, "specifies to use the MapDBStore store");
 		Option help = new Option("help", "help", false, "help print");
 		
 		options.addOption(config);
 		options.addOption(bindAddr);
 		options.addOption(path);
 		options.addOption(hmStore);
-		options.addOption(activeStore);
+		options.addOption(mdbStore);
 		options.addOption(help);
 		
 		CommandLineParser parser = new DefaultParser();
@@ -99,11 +99,11 @@ public class NodeRunner {
 			dirPath = Paths.get("./");
 		}
 		
-		Class<? extends LocalStore> storeClass = MapDBStore.class;
+		Class<? extends LocalStore> storeClass = ActiveMapDBStore.class;
 		if (cmd.hasOption(hmStore.getOpt()))
 			storeClass = HashMapStore.class;
-		if (cmd.hasOption(activeStore.getOpt()))
-			storeClass = ActiveMapDBStore.class;
+		if (cmd.hasOption(mdbStore.getOpt()))
+			storeClass = MapDBStore.class;
 		
 		if (addr != null) {
 			padNode = new Node(addr, configFile, dirPath, storeClass);
