fdb6ec6da52487b4939a86f411cce0fa1ff0b7d0
Leonardo Gazzarri <leogazza91@gmail.com>
2017-02-15 11:51:03 +0100

    remove unused imports

--- core/src/test/java/mcsn/pad/pad_fs/ActiveMapDBStoreTest.java
+++ core/src/test/java/mcsn/pad/pad_fs/ActiveMapDBStoreTest.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
